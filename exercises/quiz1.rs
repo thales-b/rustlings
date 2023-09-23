041262c7293d5ba7972aac447ba4f2d43e74b156
thales-b <thalesbouraoui@gmail.com>
2023-09-23 22:24:52 +0200

    Hash maps next

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        1 * num_apples
+    } else {
+        2 * num_apples
+    }
+}
 
 // Don't modify this function!
 #[test]
